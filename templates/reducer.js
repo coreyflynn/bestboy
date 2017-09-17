08b1a311946876a49ea0c32497af734540de6915
Corey Flynn <flynn.corey@gmail.com>
2017-09-17 10:40:57 -0400

    :art: Use createReducer instead of handleActions

--- templates/reducer.js
+++ templates/reducer.js
@@ -1,12 +1,12 @@
 // @flow
 import { Map } from 'immutable';
-import { handleActions } from 'src/utils/actions';
+import { createReducer } from 'src/utils/actions';
 import * as actions from './actions';
 
 type State = {};
 
 const defaultState: State = Map({});
 
-export default handleActions(defaultState, {
+export default createReducer(defaultState, {
   [actions.{{ name }}Action](state) {},
 });
